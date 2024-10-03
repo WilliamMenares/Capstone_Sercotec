66523b0a57605873a0a1e89b0998eab84c5532ef
Winexnix <williammenares0@gmail.com>
2024-10-03 20:26:10 -0300

    Maqueta del menu lista

--- sercotec-app/resources/views/welcome.blade.php
+++ sercotec-app/resources/views/welcome.blade.php
@@ -1,1 +1,18 @@
-Prueba para roberto
+@extends('layout.menu')
+
+@section('title','Inicio')
+
+<link rel="stylesheet" href="{{asset('css/welcome.css')}}">
+
+@section('buscador', 'welcome')
+
+
+@section('content')
+
+<div class="empleados">
+    <div class="titulo">
+        <h1>Dashboard</h1>
+    </div>
+</div>
+
+@endsection
