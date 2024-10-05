8d5c5958c3f0ed915494084e9e9f903f3240bbb9
William <wi.menares@duocuc.cl>
2024-10-05 11:35:03 -0300

    Cambios hechos en clases, vistas listas para que trabajen

--- sercotec-app/routes/web.php
+++ sercotec-app/routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::view('login', 'login')->name('login');
+Route::view('welcome', 'welcome')->name('welcome');
+Route::view('empresa', 'empresa')->name('empresa');
+Route::view('empleado', 'empleado')->name('empleado');
