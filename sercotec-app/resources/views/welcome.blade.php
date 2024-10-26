7882bb8220de3f3b9d43f5b348a0fa0eb07d4675
rezarI7I <ro.mayea@duocuc.cl>
2024-10-26 19:17:24 -0300

    checkpoint_previo_al_final

--- sercotec-app/resources/views/welcome.blade.php
+++ sercotec-app/resources/views/welcome.blade.php
@@ -2,14 +2,14 @@
 
 @section('title','Inicio')
 
-<link rel="stylesheet" href="{{asset('css/welcome.css')}}">
+<link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
 
 @section('buscador', 'welcome')
 
-
 @section('content')
     <div class="container">
-        <div class="cuadro cuadro-1">
+        <!-- Cuadro de conteo de empresas -->
+        <div class="cuadro cuadro-empresas">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Empresas</h5>
@@ -17,10 +17,47 @@
                 </div>
             </div>
         </div>
-        <div class="cuadro cuadro-2">Cuadro 2</div>
-        <div class="cuadro cuadro-3">Cuadro 3</div>
-        <div class="cuadro cuadro-4">Cuadro 4</div>
-        <div class="cuadro cuadro-5">Cuadro 5</div>
-        <div class="cuadro cuadro-6">Cuadro 6</div>
+
+        <!-- Cuadro de formularios realizados -->
+        <div class="cuadro cuadro-formularios">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Formularios Realizados</h5>
+                    <p class="card-text">Cantidad de formularios</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Cuadro de usuario logeado -->
+        <div class="cuadro cuadro-usuario">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Usuario Logeado</h5>
+                    <p class="card-text">Bienvenido, {{ Auth::user()->name }}</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Gráfico de barras -->
+        <div class="cuadro cuadro-grafico-barras">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Gráfico de Barras</h5>
+                    <p class="card-text">Aquí se mostrará el gráfico de barras.</p>
+                    <div class="grafico" id="graficoBarras"></div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Gráfico de torta -->
+        <div class="cuadro cuadro-grafico-torta">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Gráfico de Torta</h5>
+                    <p class="card-text">Aquí se mostrará el gráfico de torta.</p>
+                    <div class="grafico" id="graficoTorta"></div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
